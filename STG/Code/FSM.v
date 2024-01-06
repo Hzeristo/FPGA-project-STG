68c465d98230f9149e96d6eddd016d47dab47c82
Hzeristo <126323580+Hzeristo@users.noreply.github.com>
2024-01-06 12:10:44 +0800

    Update FSM.v

--- STG/Code/FSM.v
+++ STG/Code/FSM.v
@@ -1,6 +1,6 @@
 module FSM(
     input clk, hard_reset,
-    input enter, shift,
+    input enter, bomb,
     input collision,
     output [3:0] num_bomb,
     output [3:0] num_life,
@@ -19,16 +19,16 @@ localparam Gameover = 4'b1001;
 reg [3:0] num_bomb_next;
 reg [3:0] num_life_next;
 reg [3:0] game_state_next;
-reg game_en_next, enter_reg, shift_reg;
+reg game_en_next, enter_reg, bomb_reg;
 reg [31:0] timeout_reg, timeout_next;
 
 wire enter_posedge = enter & ~enter_reg;
-wire shift_posedge = shift & ~shift_reg;
+wire bomb_posedge = bomb & ~bomb_reg;
 
 always @(posedge clk or posedge hard_reset)
 	if(hard_reset) begin
 		enter_reg <= 0;
-        shift_reg <= 0;
+        bomb_reg <= 0;
 		game_state <= Initial;				//initialization
 		timeout_reg <= 20000000;			//reset collision timer
 		num_life <= 3;
@@ -37,7 +37,7 @@ always @(posedge clk or posedge hard_reset)
 	end
 	else begin
 		enter_reg <= enter;
-        shift_reg <= shift;jht
+        bomb_reg <= bomb;
 		game_state <= game_state_next;
 		timeout_reg <= timeout_next;
 		num_life <= life_next;
@@ -48,7 +48,70 @@ always @* begin
     game_state_next = game_state;
     timeout_next = timeout_reg;
     num_life_next = num_life;
-end
+    num_bomb_next = num_bomb;
+    game_en_next = game_en;
+    game_reset = 0;
 
+    case(game_state)
+    Initial: begin
+        if(timeout_reg > 0) begin
+            timeout_reg = timeout_reg - 1;
+        end
+        else begin
+            game_state_next = Start;
+        end
+    end
+    Start: begin
+        if(enter_posedge) begin						
+			game_en_next = 1;             	
+			game_reset = 1;            		
+			game_state_next = Play; 
+		end
+    end
+    Play: begin
+        if(bomb && num_bomb > 0) begin
+            game_state_next = Bomb;
+            timeout_next = 4000_0000_0;
+        end	
+		if(collision) begin				
+			if(num_life == 0) begin    	
+				num_life_next = num_life - 1;	
+				game_en_next = 0;          	
+				game_state_next = Gameover;
+			end
+			else begin
+				num_life_next = num_life - 1; 	
+				game_state_next = Collision;
+				timeout_next = 2000_0000_0; 	
+			end
+		end
+	end
+    Collision: begin
+        if(bomb && num_bomb > 0) begin
+            game_state_next = Bomb;
+            timeout_next = 4000_0000_0;
+        end						
+		if(timeout_reg > 0)              	
+			timeout_next = timeout_reg - 1;	
+		else 
+			game_state_next = Play;   			
+	end
+    endcase
+    Bomb: begin
+        if(timeout_reg > 0) begin
+            timeout_reg = timeout_reg - 1;
+        end
+        else begin
+            game_state_next = Play;
+        end
+    end
+    Gameover: begin
+		if(enter_posedge) begin            			
+			heart_next = 3;           			
+			game_state_next = Initial;		
+			game_reset = 1;          			
+		end
+	end
+end
 
 endmodule
