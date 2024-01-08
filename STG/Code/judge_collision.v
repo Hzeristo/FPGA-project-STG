9ec42e829bbd2910d2f257ad43c043dc82beee34
Hzeristo <126323580+Hzeristo@users.noreply.github.com>
2024-01-08 10:40:41 +0800

    judge无bug

--- STG/Code/judge_collision.v
+++ STG/Code/judge_collision.v
@@ -1,8 +1,8 @@
 module judge_collision(
     input clk, rst,
-    input [9:0] player_x, player_y;
-    input [9:0] moon_x, moon_y;
-    input [9:0] hecatia_x, hecatia_y;
+    input [9:0] player_x, player_y,
+    input [9:0] moon_x, moon_y,
+    input [9:0] hecatia_x, hecatia_y,
     output reg collision
 );
 
@@ -13,7 +13,7 @@ always @(posedge clk or posedge rst) begin
 	else begin
         if((player_x >= hecatia_x - 11 && player_x < hecatia_x + 12 && player_y >= hecatia_y - 19 && player_y < hecatia_y + 20))
             collision = 1;
-        if(((player_x - moon_x) * (player_x - moon_x) + (player_y - moon_y) * (player_y - moon_y)) < 60)
+        if(((player_x - moon_x) * (player_x - moon_x) + (player_y - moon_y) * (player_y - moon_y)) < 3600)
             collision = 1;
 	end
 end
