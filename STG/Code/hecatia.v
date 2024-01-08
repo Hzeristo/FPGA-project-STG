0bac108dd2c39561763f4eebc27dcfdfd506e6fd
Hzeristo <126323580+Hzeristo@users.noreply.github.com>
2024-01-08 17:11:27 +0800

    hecatia.v无bug

--- STG/Code/hecatia.v
+++ STG/Code/hecatia.v
@@ -3,63 +3,66 @@ module hecatia(
     input [9:0] player_x, player_y,
     input [9:0] x, y, 
     input [25:0] speed_offset,
-    input is_hit;
-    output [9:0] hecatia_x, hecatia_y,
+    input is_hit,
     output hecatia_on,
+    input [9:0] hecatia_x, hecatia_y,
     output [11:0] rgb_out,
     output [7:0] health,
     output die
 );
 
-localparam MAX_X = 384,
+localparam MAX_X = 384;
 localparam MAX_Y = 448;
-localparam TIME_MAX = 4000000;
-
+localparam TIME_MAX = 4000;
 reg [9:0] hecatia_x_reg, hecatia_y_reg;
 reg [9:0] hecatia_x_next, hecatia_y_next;
-reg [9:0] player_x_reg, player_y_reg;
 reg [25:0] time_reg;  
 wire [25:0] time_next;  
-reg tick_reg;
 assign time_next = (time_reg < TIME_MAX - speed_offset) ? time_reg + 1 : 0;        
 wire tick = (time_reg == TIME_MAX - speed_offset) ? 1 : 0;
+reg [9:0] tick_reg, tick_next;
 reg [12:0] addr_reg;
 wire [12:0] addr;
 assign addr = addr_reg;
-reg [31:0] timer_reg;
 reg [7:0] life;
+wire clk_in;
+assign clk_in = (die == 1) ? 0 : clk;
 
 initial begin
-    timer_reg = 0;
-    life = 3'd100;
+    time_reg = 0;
+    tick_next = 0;
+    life = 100;
     hecatia_x_next = 192;
     hecatia_y_next = 100;
 end
 
-always @(posedge clk or posedge reset) begin
+always @(posedge clk_in or posedge reset) begin
     if (reset) begin
         time_reg <= 0;
+        tick_reg <= 0;
     end
     else begin
         hecatia_x_reg <= hecatia_x_next;
         hecatia_y_reg <= hecatia_y_next;
-        timer_reg <= time_next;
+        time_reg <= time_next;
+        tick_reg <= tick_next;
     end
 end
 
 always @(posedge tick or posedge reset) begin
     hecatia_x_next = hecatia_x_reg;
     hecatia_x_next = hecatia_x_reg;
-    timer_reg = timer_reg + 1;
+    tick_next = tick_reg;
+    tick_next = tick_next + 1;
     if(reset) begin
         hecatia_x_next = 192;
         hecatia_y_next = 100;
     end
-    else if(timer_reg > 1000_0000_00) begin
-        hecatia_x_next = hecatia_next + 1;
+    else if(tick_reg > 100) begin
+        hecatia_x_next = hecatia_x_next + 1;
     end
-    else if(timer_reg > 1500_0000_00) begin
-        hecatia_x_next = hecatia_next - 1;
+    else if(250 > tick_reg > 150) begin
+        hecatia_x_next = hecatia_x_next - 1;
     end
     if(is_hit) begin
         life = life - 1;
