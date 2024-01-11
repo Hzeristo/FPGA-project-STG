68522f11507c8a078b9e49b568bcb36131f0ee68
Hzeristo <126323580+Hzeristo@users.noreply.github.com>
2024-01-11 15:45:38 +0800

    丰富图片资源

---
+++ STG/Code/top.v
@@ -0,0 +1,11 @@
+module Top_module(
+    input clk, rstn,        //clk & rst negative signal     
+    input SW,               //switch for controlling buzzer
+    output hsync, vsync,    //horizontal & vertical scan signal
+    output [11:0] rgb,      //rgb value to VGA port
+    output buzzer           //buzzer module
+);
+
+
+
+endmodule
